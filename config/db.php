17ca15fdb2294774e70d2c4a1b55de3f4e12c892
AceViral <sadsadas-07@mail.ru>
2022-08-04 19:42:30 +0300

    debug cors

--- config/db.php
+++ config/db.php
@@ -3,6 +3,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;port=3307;dbname=yii2basic',
     'username' => 'root',
-    'password' => '21lira05b01',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
